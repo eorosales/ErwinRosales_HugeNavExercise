e7746ddfb2f30d0a278a96ac68fd56a983de2875
Erwin Rosales <look4erwin@gmail.com>
2016-05-13 03:13:07 -0700

    Added interaction for overlay mask over content

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,36 @@
-// Rotate chevron * 180
-function rotateArrow(arrow, subDiv) {
-	if(subDiv.classList != 'dropdown-content hide') {
-		console.log('test');
-	}
-}
-
 // Add click interaction to 'hamburger'
 function setHamburgerInteraction() {
 	var hamburger = document.getElementById('logo');
 	hamburger.setAttribute('class', 'toggle-nav-button');
 	var toggleNavButton = document.querySelector('.toggle-nav-button');
 	var mainNav = document.getElementById('main-nav');
-
-	// Toggle subDiv visibility in mobile
+	var mask = document.querySelector('.overlay');
+	// Push 'n' Pull in mobile
 	toggleNavButton.addEventListener('click', function() {
 		toggleNavButton.setAttribute('href', 'javascript:void(0)');		
 		var pageContent = document.querySelector('.wrapper');
-
-		// Push primary navigation right
+		// Push primary navigation right -->
 		if(mainNav.classList != 'primary-nav primary-nav-push-right') {
 			mainNav.removeAttribute('class');
 			mainNav.setAttribute('class','primary-nav primary-nav-push-right');
+			mask.style.opacity = '1';
 		}else{
-		// Pull primary navigation left
+		// Pull primary navigation left <--
 			mainNav.removeAttribute('class');
 			mainNav.setAttribute('class', 'primary-nav primary-nav-pull-left');
+			mask.style.opacity = '0';
 		}
-
-		// Push page content to right 
+		// Push page content to right -->
 		if(pageContent.classList != 'wrapper wrapper-push-right') {
 			pageContent.removeAttribute('class');
 			pageContent.setAttribute('class', 'wrapper wrapper-push-right');
 		}else{
-		// Pull page content to left	
+		// Pull page content to left <--	
 			pageContent.removeAttribute('class');
 			pageContent.setAttribute('class', 'wrapper wrapper-pull-left');
 		}
 	});
 }
-
 // Insert arrow for items with mobile dropown content
 function appendArrow(itemsDiv, subDiv) {
 	var span = document.createElement('span');
@@ -47,38 +39,33 @@ function appendArrow(itemsDiv, subDiv) {
 	img.setAttribute('src', '/images/arrow.png');
 	span.appendChild(img);
 	span.setAttribute('class', 'chevron-rotate-down');
-	
 	arrow.firstChild.appendChild(span);
-	rotateArrow(arrow, subDiv);
-
 	return arrow;
 }
-
 // Toggle push and pull for subDiv
 function toggleDropdownContent (itemsDiv, subDiv) {
-	var arrow = appendArrow(itemsDiv, subDiv); // Add arrows to items with secondary items
+	var arrow = appendArrow(itemsDiv, subDiv); // Add arrows to items with dropdown content
+	var mask = document.querySelector('.overlay');
 	itemsDiv.addEventListener('click', function() {		
 		if(itemsDiv.firstChild.nextSibling.classList.contains('hide')) {
 			subDiv.removeAttribute('class');
 			subDiv.setAttribute('class', 'dropdown-content show');
-			
-			// Rotate chevron into initial position
+			mask.style.opacity = '1';	
+			// Rotate chevron into initial position depending on current class of subDiv
 			var chevron = this.querySelector('.chevron-rotate-down');
 			chevron.removeAttribute('class');
-			chevron.setAttribute('class', 'chevron-rotate-up');
-
+			chevron.setAttribute('class', 'chevron-rotate-up');	
 		}else{
 			subDiv.removeAttribute('class');
 			subDiv.setAttribute('class', 'dropdown-content hide');
-
-			// Rotate chevron 180 degrees
+			mask.style.opacity = '0';
+			// Rotate chevron 180 degrees depending on current class of subDiv
 			var chevron = this.querySelector('.chevron-rotate-up');
 			chevron.removeAttribute('class');
 			chevron.setAttribute('class', 'chevron-rotate-down');
 		}
 	});
 }
-
 // Construct div container to hold secondary items
 function setSubNav(itemsDiv, subItemsLength, subItems) {
 	if(typeof(subItems) !== 'undefined') {
@@ -95,49 +82,39 @@ function setSubNav(itemsDiv, subItemsLength, subItems) {
 				subDiv.setAttribute('class', 'dropdown-content hide');
 				itemsDiv.appendChild(subDiv);
 			}	
-			toggleDropdownContent(itemsDiv, subDiv); // Toggle mobile navigation visibility
-
+			toggleDropdownContent(itemsDiv, subDiv); // Toggle dropdown content for mobile
 			return subDiv;
 		}
 	}
 }
-
 // Construct div elements to hold items
 function setListItems(items, label, url) {
 	var itemsDiv = document.createElement('div');
 	var itemsUrl = document.createElement('a');
 	var itemsUrlText = document.createTextNode(label);
-
 	itemsUrl.appendChild(itemsUrlText);
 	itemsUrl.setAttribute('href', url);
 	itemsDiv.appendChild(itemsUrl);
 	itemsDiv.setAttribute('class', 'top-nav-item');
-
 	return itemsDiv;	
 }
-
 // Set labels and urls to vars
 function setItems(items) {
 	var itemsLength = items.length;
 	var subClick = items.items;
 	var mainNavContainer = document.getElementById('main-nav');
-
 	for(var i = 0; i < itemsLength; i++) {
-
 		var itemsLabel = items[i].label;
 		var itemsUrl = items[i].url;
 		var itemsDiv = setListItems(items, itemsLabel, itemsUrl);
-
 		var subItems = items[i].items;
 		var subItemsLength = items[i].items.length;
 		var subDiv = setSubNav(itemsDiv, subItemsLength, subItems);
-
 		mainNavContainer.appendChild(itemsDiv);
 	}
 	setHamburgerInteraction();
 	return itemsDiv;
 }
-
 // Fire AJAX request
 function accessNavItems () {
 	var xhr = new XMLHttpRequest();
@@ -151,5 +128,4 @@ function accessNavItems () {
 	xhr.open('GET', '/api/nav.json', true);
 	xhr.send();
 }
-
 accessNavItems();
